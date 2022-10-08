4d189756dea01da8f3d7e08ea590af8eebd01b3b
wverbeeck <49304641+wverbeeck@users.noreply.github.com>
2022-10-08 10:44:04 +0200

    and to Or

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ let degrees = 0
 radio.setGroup(1)
 basic.forever(function () {
     degrees = input.compassHeading()
-    if (degrees >= 345 && degrees <= 15) {
+    if (degrees >= 345 || degrees <= 15) {
         basic.showArrow(ArrowNames.North)
         radio.sendMessage(RadioMessage.North)
     } else {
